b5af3f1675219398998e3288cb1211e859ab6515
HanxinHua <HanxinHua@github.com>
2018-11-16 14:44:32 -0500

    deploy finished

--- test_all.py
+++ test_all.py
@@ -1,6 +1,8 @@
 from validate import *
 import pytest
 import datetime
+from is_tachycardia import is_tachycardia
+from average import calculate_average
 
 
 @pytest.mark.parametrize("email,expected", [
@@ -112,4 +114,47 @@ def test_validate_heart_rate_request(r_dic, expected):
     ])
 def test_validate_interval_average_request(r_dic, expected):
     response = validate_interval_average_request(r_dic)
+    assert response == expected
+
+
+@pytest.mark.parametrize("age, heart_rate,expected", [
+    (50, 101, True),
+    (40, 100, False),
+    (15, 120, True),
+    (12, 119, False),
+    (11, 131, True),
+    (8, 130, False),
+    (7, 134, True),
+    (5, 133, False),
+    (4, 138, True),
+    (3, 137, False),
+    (2, 152, True),
+    (1, 151, False),
+    (0.9, 170, True),
+    (0.5, 169, False),
+    (0.4, 187, True),
+    (0.25, 186, False),
+    (1/12, 180, True),
+    (2/12, 179, False),
+    (7/356, 183, True),
+    (8/356, 182, False),
+    (3/356, 167, True),
+    (4/356, 166, False),
+    (2/356, 160, True),
+    (1/356, 159, False),
+    ])
+def test_is_tachycardia(age, heart_rate, expected):
+    response = is_tachycardia(heart_rate, age)
+    assert response == expected
+
+
+@pytest.mark.parametrize("data, time, since, expected", [
+    ([1, 2, 3], [1, 2, 3], 1, 2),
+    ([1, 2, 3], [1, 2, 3], 2, 2.5),
+    ([1, 2, 3], [1, 2, 3], 1.5, 2.5),
+    ([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], 1, 3.5),
+    ([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], 3.5, 5),
+    ])
+def test_average(data, time, since, expected):
+    response = calculate_average(data, time, since)
     assert response == expected
